f066317c1964de9c3aaeae7b41f787323413da64
Simon Sagi <github@simonsagi.com>
2016-09-14 20:39:27 +0200

    - Setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'fnversion',
+  packages = ['fnversion'],
+  version = '1.0',
+  description = 'Small tool to support multiple versions of the same function or method in Python.',
+  author = 'Simon Sagi',
+  author_email = 'fnversion@simonsagi.com',
+  url = 'https://github.com/tejfel/fnversion',
+  download_url = 'https://github.com/tejfel/fnversion/tarball/1.0',
+  keywords = ['version', 'versioning', 'method', 'function'],
+  classifiers = [],
+)
